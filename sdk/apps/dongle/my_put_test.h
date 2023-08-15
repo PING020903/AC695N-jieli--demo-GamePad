4d81250f9f9840c1bceac107c056db99d672db19
平 <88314322+PING020903@users.noreply.github.com>
2023-08-15 11:11:17 +0800

    更正了函数声明

--- sdk/apps/dongle/my_put_test.h
+++ sdk/apps/dongle/my_put_test.h
@@ -22,22 +22,24 @@ enum {
     CHECK_KEY_TASK= (Q_USER + 3),
 };
 
-void my_send_ADkeyX_event(void);
-void my_send_ADkeyY_event(void);
-void my_send_IOkey_event(void);
 
+void my_read_key(void);             // 读取IO按键按键的状态, 由于是要上拉, 故此读取后需取反
+void read_trigger_value(void);      // 读取扳机的AD输入值, 该值由vbat对比而来
+void left_read_rocker(void);        // 读取左摇杆的AD输入值
+void right_read_rocker(void);       // 读取右摇杆的AD输入值
 
-void* my_timeout_task(void* p_arg);
-void* my_task(void* p_arg);
-void my_read_key(void);
-void my_led_function(void);
-void my_task_init(void);
+void my_led_function(void);         // 实际上是PWM输出函数, 试验完毕后懒得改名字, 用于处理motor的震动
 
-/*merge io key*/
-unsigned char abc(unsigned char all_key, unsigned char key, unsigned int bit);
+void connect_flicker(void);         // 连接成功后player指示灯熄灭亮起4次
 
-void* input_task(void* p_arg);
-void input_task_init(void);
+void my_button_init(void);          // IO初始化
+void my_PWM_output_init(void);      // PWM输出初始化
+
+void* my_timer_task(void* p_arg);   // 主要任务定时器
+void* led_timer_task(void* p_arg);  // PWM输出任务定时器
+
+void* my_task(void* p_arg);         // 主线程
+void my_task_init(void);            // 线程初始化
 
 
 #endif // _MY_PUT_TEST_
