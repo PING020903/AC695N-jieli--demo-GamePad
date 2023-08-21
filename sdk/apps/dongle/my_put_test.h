3122b0cdda3cf65fa060f532df7d9bff0a9f739f
平 <88314322+PING020903@users.noreply.github.com>
2023-08-21 15:00:05 +0800

    准备添加普通按键可编程功能

--- sdk/apps/dongle/my_put_test.h
+++ sdk/apps/dongle/my_put_test.h
@@ -17,10 +17,11 @@
 
 
 enum {
-    MAIN_TCC_TASK = (Q_USER + 1),
-    BREATHE_LED_TASK= (Q_USER + 2),
-    CHECK_KEY_TASK= (Q_USER + 3),
+    MAIN_TCC_TASK       = (Q_USER + 1),
+    BREATHE_LED_TASK,
+    SPECIAL_FUNCTIONS,
 };
+
 
 
 void my_read_key(void);             // 读取IO按键按键的状态, 由于是要上拉, 故此读取后需取反
@@ -32,6 +33,8 @@ void my_led_function(void);         // 实际上是PWM输出函数, 试验完毕
 
 void connect_flicker(void);         // 连接成功后player指示灯熄灭亮起4次
 
+void records_movement(void);        // 记录按键行动
+
 void my_button_init(void);          // IO初始化
 void my_PWM_output_init(void);      // PWM输出初始化
 
@@ -41,6 +44,7 @@ void* led_timer_task(void* p_arg);  // PWM输出任务定时器 */
 
 void* my_task(void* p_arg);         // 主线程
 void my_task_init(void);            // 线程初始化
+
 
 
 #endif // _MY_PUT_TEST_
