97918173bbe61b9020abc10a5e4e0bd01ee8cf00
平 <88314322+PING020903@users.noreply.github.com>
2023-09-13 11:44:39 +0800

    增加對PS3的支持

--- sdk/apps/common/usb/device/descriptor.c
+++ sdk/apps/common/usb/device/descriptor.c
@@ -16,13 +16,18 @@
 #define LOG_ERROR_ENABLE
 #define LOG_DEBUG_ENABLE
 #define LOG_INFO_ENABLE
+
+#define PC_XBOX360  1   // support PC only, Xbox360适配
+#define HOST_PS3    1   // PS3适配
 /* #define LOG_DUMP_ENABLE */
 #define LOG_CLI_ENABLE
 #include "debug.h"
 
+unsigned char my_device_type = 0;
+extern unsigned char usb_connect_timeout_flag;  // from task_pc.c
 #if TCFG_USB_SLAVE_ENABLE
 
-#if 0//旧的设备描述
+#if DISABLE//旧的设备描述
 static const u8 sDeviceDescriptor[] = { //<Device Descriptor
     USB_DT_DEVICE_SIZE,      // bLength: Size of descriptor
     USB_DT_DEVICE,       // bDescriptorType: Device
@@ -46,7 +51,8 @@ static const u8 sDeviceDescriptor[] = { //<Device Descriptor
     0x01        // bNumConfigurations: 1(更正为 2, 因为添加HID设备-手柄)
 };
 #endif // 0
-#if 1//新的设备描述
+
+#if PC_XBOX360
 static const u8 sDeviceDescriptor[] = { //<Device Descriptor
     USB_DT_DEVICE_SIZE,         // bLength: Size of descriptor  0x12
     USB_DT_DEVICE,              // bDescriptorType: Device      0x01
@@ -57,7 +63,7 @@ static const u8 sDeviceDescriptor[] = { //<Device Descriptor
 #else
 #error "USB_SPEED_MODE not defined"
 #endif
-    0xff,       // bDeviceClass: none(填00意味着从接口描述符中获取这个'类信息')
+    0xff,       // bDeviceClass: none('0x00'从接口描述符中获取这个'类信息', '0xff'供应商定义)
     0xff,       // bDeviceSubClass: none
     0xff,       // bDeviceProtocol: none
     EP0_SETUP_LEN,//EP0_LEN,      // bMaxPacketSize0: 8/64 bytes    0x40
@@ -67,14 +73,60 @@ static const u8 sDeviceDescriptor[] = { //<Device Descriptor
     0x01,       // iManufacturer: Index to string descriptor that contains the string <Your Name> in Unicode
     0x02,       // iProduct: Index to string descriptor that contains the string <Your Product Name> in Unicode
     0x03,       // iSerialNumber: none
-    0x01        // bNumConfigurations: 1(更正为 2, 因为添加HID设备-手柄)
+    0x01        // bNumConfigurations: 1
 };
-#endif // 1
+
+static const u8 sConfigDescriptor[0x09] = 	// < Config Descriptor >
+{
+    //bNumInterfaces: 在set_descriptor函数里面计算
+    0x09,           // 长度
+    0x02,           // 类型
+    0x90, 0x00,     // 总长度(占两字节)
+    0x04,           // 接口数量
+    0x01,           // 配置的值
+    0x00,           // 配置字符串索引
+    0xa0,           // 配置的属性
+    0xfa            // 需要的电流
+};
+#endif
+
+#if HOST_PS3
+static const u8 ps3_DeviceDescriptor[] = { //<Device Descriptor
+    USB_DT_DEVICE_SIZE,         // bLength: Size of descriptor  0x12
+    USB_DT_DEVICE,              // bDescriptorType: Device      0x01
+#if defined(FUSB_MODE) && FUSB_MODE
+    0x10, 0x01,     // bcdUSB: USB 1.0
+#endif
+    0x00,       // bDeviceClass: none(填00意味着从接口描述符中获取这个'类信息')
+    0x00,       // bDeviceSubClass: none
+    0x00,       // bDeviceProtocol: none
+    EP0_SETUP_LEN,//EP0_LEN,      // bMaxPacketSize0: 8/64 bytes    0x40
+    0x4c, 0x05,     // idVendor:
+    0x68, 0x02,     // idProduct: chip id
+    0x00, 0x01,     // bcdDevice: version 1.0
+    0x01,       // iManufacturer: Index to string descriptor that contains the string <Your Name> in Unicode
+    0x02,       // iProduct: Index to string descriptor that contains the string <Your Product Name> in Unicode
+    0x00,       // iSerialNumber: none
+    0x01        // bNumConfigurations: 1
+};
+
+static const u8 ps3_ConfigDescriptor[0x09] = 	// < Config Descriptor >
+{
+    //bNumInterfaces: 在set_descriptor函数里面计算
+    0x09,           // 长度
+    0x02,           // 类型
+    0x29, 0x00,     // 总长度(占两字节)
+    0x01,           // 接口数量
+    0x01,           // 配置的值
+    0x00,           // 配置字符串索引
+    0x80,           // 配置的属性
+    0xfa            // 需要的电流
+};
+#endif
 
 static const u8 LANGUAGE_STR[] = {
     0x04, 0x03, 0x09, 0x04
 };
-
 
 static const u8 product_string[] = {
     42,
@@ -122,14 +174,6 @@ static const u8 MANUFACTURE_STR[] = {
     0x79, 0x00, //y
 };
 
-
-static const u8 sConfigDescriptor[0x09] = 	// < Config Descriptor >
-{
-    //bNumInterfaces: 在set_descriptor函数里面计算
-    0x09, 0x02, 0x90, 0x00, 0x04, 0x01, 0x00, 0xa0, 0xfa
-};
-
-
 static const u8 serial_string[] = {
     0x22, 0x03,
     0x30, 0x00,
@@ -150,10 +194,25 @@ static const u8 serial_string[] = {
     0x37, 0x00
 };
 
+/* 判断连接超时进行设备描述符的切换 */
+/* default: PC */
 void get_device_descriptor(u8 *ptr)
 {
-    printf("---------- %s ----------\n", __func__);
-    memcpy(ptr, sDeviceDescriptor, USB_DT_DEVICE_SIZE);
+
+    printf("---------- %s ----------, %d\n", __func__, usb_connect_timeout_flag);
+    // 与主机首次通讯时, 主机并不清楚从机是个什么设备, 任意设备描述符即可
+    if (usb_connect_timeout_flag == 1)    // PC适配
+    {
+        printf(" PC device_descriptor ! ! !");
+        memcpy(ptr, sDeviceDescriptor, USB_DT_DEVICE_SIZE);
+    }
+
+    if (usb_connect_timeout_flag == 2)    // PS3适配
+    {
+        printf(" PS3 device_descriptor ! ! !");
+        memcpy(ptr, ps3_DeviceDescriptor, USB_DT_DEVICE_SIZE);
+    }
+
 }
 
 void get_language_str(u8 *ptr)
@@ -216,12 +275,19 @@ void get_string_ee(u8 *ptr)
 void get_product_str(u8 *ptr)
 {
     memcpy(ptr, product_string, product_string[0]);
+    log_debug("-------- %s --------", __func__);
 }
 
+/* 判断连接超时进行配置描述符的切换 */
+/* default: PC */
 const u8 *usb_get_config_desc()
 {
     log_debug("---------- %s ----------", __func__);
-    return sConfigDescriptor;
+
+    if (usb_connect_timeout_flag == 1) // PC适配
+        return sConfigDescriptor;
+    if (usb_connect_timeout_flag == 2) // PS3适配
+        return ps3_ConfigDescriptor;
 }
 
 const u8 *usb_get_string_desc(u32 id)
