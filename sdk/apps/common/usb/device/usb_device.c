b4b9140167cbee6ac9905c66d5ebf67a371b4cbb
平 <88314322+PING020903@users.noreply.github.com>
2023-09-13 11:45:46 +0800

    增加對PS3的支持

--- sdk/apps/common/usb/device/usb_device.c
+++ sdk/apps/common/usb/device/usb_device.c
@@ -19,13 +19,17 @@
 #define LOG_CLI_ENABLE
 
 #include "debug.h"
+#include "timer.h"
 
 /*********user*********/
 u32 xbox360_register(const usb_dev usb_id);
-void uac_my_pc360_0(const usb_dev usb_id, u8 *ptr, u32 *cur_itf_num);
-void uac_my_pc360_1(const usb_dev usb_id, u8 *ptr, u32 *cur_itf_num);
-void uac_my_pc360_2(const usb_dev usb_id, u8 *ptr, u32 *cur_itf_num);
-void uac_my_pc360_3(const usb_dev usb_id, u8 *ptr, u32 *cur_itf_num);
+u32 uac_my_pc360_0(const usb_dev usb_id, u8 *ptr, u32 *cur_itf_num);
+u32 uac_my_pc360_1(const usb_dev usb_id, u8 *ptr, u32 *cur_itf_num);
+u32 uac_my_pc360_2(const usb_dev usb_id, u8 *ptr, u32 *cur_itf_num);
+u32 uac_my_pc360_3(const usb_dev usb_id, u8 *ptr, u32 *cur_itf_num);
+
+u32 ps3_register(const usb_dev usb_id);
+u32 hid_my_ps3_0(const usb_dev usb_id, u8* ptr, u32* cur_itf_num);
 /**********************/
 
 #if TCFG_USB_SLAVE_ENABLE
@@ -49,7 +53,7 @@ static void usb_device_init(const usb_dev usb_id)
     usb_clr_intr_rxe(usb_id, -1);
     usb_set_intr_txe(usb_id, 0);
     usb_set_intr_rxe(usb_id, 0);
-    usb_g_isr_reg(usb_id, 3, 0);
+    usb_g_isr_reg(usb_id, 3, 0);    // 配置USB的响应级别与中断函数
     /* usb_sof_isr_reg(usb_id,3,0); */
     /* usb_sofie_enable(usb_id); */
     /* USB_DEBUG_PRINTF("ep0 addr %x %x\n", usb_get_dma_taddr(0), ep0_dma_buffer); */
@@ -62,9 +66,136 @@ static void usb_device_hold(const usb_dev usb_id)
 }
 
 
-
 static int usb_ep_conflict_check(const usb_dev usb_id);
 int usb_device_mode(const usb_dev usb_id, const u32 class)
+{
+    log_debug("%s, class: %d", __func__, class);
+    extern unsigned char usb_connect_timeout_flag;
+
+    /* usb_device_set_class(CLASS_CONFIG); */
+    u8 class_index = 0;
+    if (class == 0) {   // demo中三种模式(SPEAKER, MIC, HID)没有任何模式启用
+        gpio_direction_input(IO_PORT_DM + 2 * usb_id);
+        gpio_set_pull_up(IO_PORT_DM + 2 * usb_id, 0);
+        gpio_set_pull_down(IO_PORT_DM + 2 * usb_id, 0);
+        gpio_set_die(IO_PORT_DM + 2 * usb_id, 0);
+
+        gpio_direction_input(IO_PORT_DP + 2 * usb_id);
+        gpio_set_pull_up(IO_PORT_DP + 2 * usb_id, 0);
+        gpio_set_pull_down(IO_PORT_DP + 2 * usb_id, 0);
+        gpio_set_die(IO_PORT_DP + 2 * usb_id, 0);
+
+        os_time_dly(15);
+
+        gpio_set_die(IO_PORT_DM + 2 * usb_id, 1);
+        gpio_set_die(IO_PORT_DP + 2 * usb_id, 1);
+        printf("------ SET USB IO ------ ! ! !");
+
+#if TCFG_USB_SLAVE_MSD_ENABLE
+        msd_release(usb_id);
+#endif
+
+#if TCFG_USB_SLAVE_AUDIO_ENABLE
+        uac_release(usb_id);
+#endif
+
+#if TCFG_USB_SLAVE_CDC_ENABLE
+        cdc_release(usb_id);
+#endif
+#if TCFG_USB_SLAVE_HID_ENABLE
+        hid_release(usb_id);
+#endif
+        usb_device_hold(usb_id);
+        return 0;
+    }
+
+    /* int ret = usb_ep_conflict_check(usb_id); */
+    /* if (ret) {                               */
+    /*     return ret;                          */
+    /* }                                        */
+    log_debug(" Before usb memory init ! ! !");
+    usb_memory_init();
+    log_debug("usb memory init");
+    usb_add_desc_config(usb_id, MAX_INTERFACE_NUM, NULL);
+    log_debug("usb add desc config, first");
+#if 0//TCFG_USB_SLAVE_MSD_ENABLE
+    if ((class & MASSSTORAGE_CLASS) == MASSSTORAGE_CLASS) {
+        log_info("add desc msd");
+        usb_add_desc_config(usb_id, class_index++, msd_desc_config);
+        msd_register(usb_id);
+    }
+#endif
+
+
+#if DISABLE
+#if TCFG_USB_SLAVE_AUDIO_ENABLE
+    if ((class & AUDIO_CLASS) == AUDIO_CLASS) {
+        log_info("add audio desc");
+        usb_add_desc_config(usb_id, class_index++, uac_audio_desc_config);
+        uac_register(usb_id);//翻译虽是登记或寄存器，实质上应该是做缓冲区预处理
+
+
+
+    } else if ((class & SPEAKER_CLASS) == SPEAKER_CLASS) {
+        log_info("add desc speaker");
+        usb_add_desc_config(usb_id, class_index++, uac_spk_desc_config);
+        uac_register(usb_id);
+    } else if ((class & MIC_CLASS) == MIC_CLASS) {
+        log_info("add desc mic");
+        usb_add_desc_config(usb_id, class_index++, uac_mic_desc_config);
+        uac_register(usb_id);
+    }
+#endif
+#endif // DISABLE
+
+#if ENABLE
+    
+
+    if (usb_connect_timeout_flag == 1)
+    {
+        // 执行到该处时, 主机尚未发送索要设备描述符命令
+        xbox360_register(usb_id);   // 缓冲区预处理
+        usb_add_desc_config(usb_id, class_index++, uac_my_pc360_0); // 将接口函数作为usb_add_desc_config的回调函数
+        usb_add_desc_config(usb_id, class_index++, uac_my_pc360_1);
+        usb_add_desc_config(usb_id, class_index++, uac_my_pc360_2);
+        usb_add_desc_config(usb_id, class_index++, uac_my_pc360_3);
+        printf("merge PC configuration descriptor ! ! !");
+        // 经过此处后才开始连接USB
+    }
+
+    if (usb_connect_timeout_flag == 2)
+    {
+        ps3_register(usb_id);
+        usb_add_desc_config(usb_id, class_index++, hid_my_ps3_0);
+        printf("merge PS3 configuration descriptor ! ! !");
+    }
+#endif
+
+
+#if DISABLE //TCFG_USB_SLAVE_HID_ENABLE
+    if ((class & HID_CLASS) == HID_CLASS) {
+        log_info("add desc std hid");
+        hid_register(usb_id);
+        usb_add_desc_config(usb_id, class_index++, hid_desc_config);
+    }
+#endif
+
+
+#if 0//TCFG_USB_SLAVE_CDC_ENABLE
+    if ((class & CDC_CLASS) == CDC_CLASS) {
+        log_info("add desc cdc");
+        usb_add_desc_config(usb_id, class_index++, cdc_desc_config);
+        cdc_register(usb_id);
+    }
+#endif
+
+    usb_device_init(usb_id);
+    user_setup_filter_install(usb_id2device(usb_id));
+    return 0;
+}
+/*********************user*********************/
+#if 0
+int ps3_usb_device(const usb_dev usb_id, const u32 class)
 {
     log_debug("%s", __func__);
     /* usb_device_set_class(CLASS_CONFIG); */
@@ -103,80 +234,15 @@ int usb_device_mode(const usb_dev usb_id, const u32 class)
         return 0;
     }
 
-    /* int ret = usb_ep_conflict_check(usb_id); */
-    /* if (ret) {                               */
-    /*     return ret;                          */
-    /* }                                        */
-
     usb_memory_init();
-
     usb_add_desc_config(usb_id, MAX_INTERFACE_NUM, NULL);
-
-#if 0//TCFG_USB_SLAVE_MSD_ENABLE
-    if ((class & MASSSTORAGE_CLASS) == MASSSTORAGE_CLASS) {
-        log_info("add desc msd");
-        usb_add_desc_config(usb_id, class_index++, msd_desc_config);
-        msd_register(usb_id);
-    }
-#endif
-
-
-
-#if DISABLE
-#if TCFG_USB_SLAVE_AUDIO_ENABLE
-    if ((class & AUDIO_CLASS) == AUDIO_CLASS) {
-        log_info("add audio desc");
-        usb_add_desc_config(usb_id, class_index++, uac_audio_desc_config);
-        uac_register(usb_id);//翻译虽是登记或寄存器，实质上应该是做缓冲区预处理
-
-
-
-    } else if ((class & SPEAKER_CLASS) == SPEAKER_CLASS) {
-        log_info("add desc speaker");
-        usb_add_desc_config(usb_id, class_index++, uac_spk_desc_config);
-        uac_register(usb_id);
-    } else if ((class & MIC_CLASS) == MIC_CLASS) {
-        log_info("add desc mic");
-        usb_add_desc_config(usb_id, class_index++, uac_mic_desc_config);
-        uac_register(usb_id);
-    }
-#endif
-#endif // DISABLE
-
-#if ENABLE
-    log_info("add desc my xbox360");
-    xbox360_register(usb_id);//缓冲区预处理
-
-    usb_add_desc_config(usb_id, class_index++, uac_my_pc360_0);
-    usb_add_desc_config(usb_id, class_index++, uac_my_pc360_1);
-    usb_add_desc_config(usb_id, class_index++, uac_my_pc360_2);
-    usb_add_desc_config(usb_id, class_index++, uac_my_pc360_3);
-
-
-#endif // MY_GAME_PAD
-
-
-#if DISABLE //TCFG_USB_SLAVE_HID_ENABLE
-    if ((class & HID_CLASS) == HID_CLASS) {
-        log_info("add desc std hid");
-        hid_register(usb_id);
-        usb_add_desc_config(usb_id, class_index++, hid_desc_config);
-    }
-#endif
-
-
-#if 0//TCFG_USB_SLAVE_CDC_ENABLE
-    if ((class & CDC_CLASS) == CDC_CLASS) {
-        log_info("add desc cdc");
-        usb_add_desc_config(usb_id, class_index++, cdc_desc_config);
-        cdc_register(usb_id);
-    }
-#endif
 
     usb_device_init(usb_id);
     user_setup_filter_install(usb_id2device(usb_id));
     return 0;
 }
+#endif
+/**********************************************/
 /* module_initcall(usb_device_mode); */
 
 static int usb_ep_conflict_check(const usb_dev usb_id)
